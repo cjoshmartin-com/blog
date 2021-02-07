5fe0509ccf69734b56da8a84286ea6dacc162260
joshmartin5001@gmail.com <Josh Martin>
2021-02-06 18:31:26 -0600

    updated bio and colors

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -9,6 +9,6 @@ export default () => (
   <Fragment>
     <Styled.a href="https://cjoshmartin.com">Josh Martin</Styled.a>
     <br />
-    Software Engineer
+      Full Stack Software Engineer, based in Chicago, IL, writing software in Django, Vue.js and React/React Native. 
   </Fragment>
 )
